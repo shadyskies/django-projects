64cc376c98ce5fddf0fab443d9f739d4debbce55
shadyskies <mehta.naman328@gmail.com>
2021-04-05 00:37:08 +0530

    refactored urls and added permissions to api

--- api_tutorial/myapi/views.py
+++ api_tutorial/myapi/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from .serializers import HeroSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Hero
+from snippets.permissions import IsOwnerorReadOnly
 
 
 class HeroViewSet(viewsets.ModelViewSet):
     queryset = Hero.objects.all().order_by('name')
     serializer_class = HeroSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerorReadOnly]
--- api_tutorial/snippets/urls.py
+++ api_tutorial/snippets/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from snippets import views
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.routers import DefaultRouter
-
+from myapi import views as myapi_views
 
 '''urlpatterns = format_suffix_patterns([
     path('', views.api_root),
@@ -27,6 +27,7 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
 router.register(r'users', views.UserViewSet)
+router.register(r'heros', myapi_views.HeroViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
